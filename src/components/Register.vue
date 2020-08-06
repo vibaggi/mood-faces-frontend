13feffb71b441df84e1169b05fb277bceca31ee1
Vitor Baggi de Almeida <vitor_baggi@hotmail.com>
2020-08-06 00:12:28 -0300

    preparando tela de login com IBM Carbon

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,24 +1,50 @@
 <template>
-    <div id="register">
-        <h3>Crie uma conta</h3>
-        <div id="form">
-            <input type="text" placeholder="login">
-            <input type="password" placeholder="senha">
-            <input type="password" placeholder="confirmar senha">
-            <button v-bind:disabled="disabledButton">Criar</button>
+    <div id="register" class="bx--grid">
+        <div class="bx--row">
+            <div class="bx--col-md-3"></div>
+            <div class="bx--col-md-2">
+                <h3>Crie uma conta</h3>
+            </div>
+            <div class="bx--col-md-3"></div>
+        </div>
+        <div class="bx--row">
+            <div class="bx--col-md-3"></div>
+            <cv-form class="bx--col-md-2" @submit.prevent="actionSubmit">
+                <cv-text-input id="login" label="Login" placeholder="digite seu nome de usuário"></cv-text-input>
+                <cv-text-input id="senha" theme="" label="Senha" type="password" passwordHideLabel="Esconder Senha" passwordShowLabel="Exibir senha" placeholder="digite a senha"></cv-text-input>
+                <cv-text-input id="confSenha" theme="" label="Confirmar Senha" type="password" passwordHideLabel="Esconder Senha" passwordShowLabel="Exibir senha" placeholder="digite a senha"></cv-text-input>
+                <cv-button >Criar</cv-button>
+            </cv-form>
+            <div class="bx--col-md-3"></div>
         </div>
     </div>
 </template>
 
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Register extends Vue {
 
-    disabledButton = true
-
+    form = {
+        login: '',
+        senha: '',
+        confirmarSenha: ''
+    }
+    actionSubmit(evento){
+        console.log(evento);
+    }
+    disabledButton(){
+        return !(this.form.login != '' && this.form.senha != '' && this.form.senha == this.form.confirmarSenha)
+    }
 }
 
 </script>
+
+<style lang="scss">
+@import 'src/styles/_carbon-utils';
+h3{
+    margin: $spacing-09 0;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import CarbonComponentsVue from "@carbon/vue";
 
 Vue.config.productionTip = false
+Vue.use(CarbonComponentsVue);
 
 new Vue({
   router,
