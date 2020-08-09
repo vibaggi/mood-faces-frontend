ae21628d286914896bcfe9c69fb0ad689ac0766b
Vitor Baggi de Almeida <vitor_baggi@hotmail.com>
2020-08-09 11:54:01 -0300

    NavBar & SideBar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,49 @@
 <template>
   <div class="home">
-    <SideBar/>
+    <cv-header aria-label="Carbon header">
+      <cv-skip-to-content href="#main-content">Skip to content</cv-skip-to-content>
+      <cv-header-name href="javascript:void(0)" prefix="Mood Faces">[Platform]</cv-header-name>
+      <template slot="header-global">
+        <cv-header-global-action aria-label="Criar Equipe" aria-controls="events-panel" @click="actionTeams" >
+          <Events32 />
+        </cv-header-global-action>
+
+      </template>
+      <template slot="left-panels">
+        <cv-side-nav id="side-nav">
+          <cv-side-nav-items>
+            <cv-side-nav-menu title="Equipes" expanded>
+              <cv-side-nav-menu-item v-for="equipe in equipes" :key="equipe">{{equipe}}</cv-side-nav-menu-item>
+            </cv-side-nav-menu>
+            <cv-side-nav-menu title="Dashbords" expanded>
+              <cv-side-nav-menu-item href="javascript:void(0)">Seu desempenho</cv-side-nav-menu-item>
+            </cv-side-nav-menu>
+            <cv-side-nav-link href="javascript:void(0)">Ajuda</cv-side-nav-link>
+            <cv-side-nav-link href="javascript:void(0)">Sobre</cv-side-nav-link>
+          </cv-side-nav-items>
+        </cv-side-nav>
+      </template>
+    </cv-header>
+    
   </div>
 </template>
 
 <script>
-import SideBar from '@/components/SideBar'
+import Events32 from '@carbon/icons-vue/es/events/32'
 export default {
-  name: 'Home',
+  name: "Home",
+  computed: {
+    equipes() {
+      return ["ABC", "DEF", "GHI"];
+    }
+  },
   components: {
-    SideBar
+    Events32
+  },
+  methods: {
+    actionTeams(){
+      console.log("Criando Equipe");
+    }
   }
-}
+};
 </script>
